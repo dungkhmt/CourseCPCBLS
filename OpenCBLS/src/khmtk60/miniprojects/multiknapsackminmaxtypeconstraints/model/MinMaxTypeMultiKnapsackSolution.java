fea6e9ca8e2ae3e5b11fc3ffda89da408e1ebd10
dungkhmt <dungkhmt@gmail.com>
2019-03-12 00:14:21 +0700

    start

---
+++ OpenCBLS/src/khmtk60/miniprojects/multiknapsackminmaxtypeconstraints/model/MinMaxTypeMultiKnapsackSolution.java
@@ -0,0 +1,10 @@
+package khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model;
+
+public class MinMaxTypeMultiKnapsackSolution {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
