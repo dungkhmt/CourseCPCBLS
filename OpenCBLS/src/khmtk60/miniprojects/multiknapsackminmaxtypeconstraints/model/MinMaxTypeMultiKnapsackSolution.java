7ed183a3bd360979d133b2a18297629dcca3c71b
dungkhmt <dungkhmt@gmail.com>
2019-05-03 11:13:21 +0700

    design solution

--- OpenCBLS/src/khmtk60/miniprojects/multiknapsackminmaxtypeconstraints/model/MinMaxTypeMultiKnapsackSolution.java
+++ OpenCBLS/src/khmtk60/miniprojects/multiknapsackminmaxtypeconstraints/model/MinMaxTypeMultiKnapsackSolution.java
@@ -1,7 +1,16 @@
 package khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model;
 
 public class MinMaxTypeMultiKnapsackSolution {
+	private int[] binOfItem;// binOfItem[i] = -1: item i not scheduled
 	
+	public int[] getBinOfItem() {
+		return binOfItem;
+	}
+
+	public void setBinOfItem(int[] binOfItem) {
+		this.binOfItem = binOfItem;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
