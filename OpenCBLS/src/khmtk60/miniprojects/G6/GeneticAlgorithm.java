4fe556ef622b6153298684c71e78dffa990ab333
Bui Duc Hoa <33179823+luxifeo@users.noreply.github.com>
2019-05-09 22:44:38 +0700

    Nhom 6

--- OpenCBLS/src/khmtk60/miniprojects/G6/GeneticAlgorithm.java
+++ OpenCBLS/src/khmtk60/miniprojects/G6/GeneticAlgorithm.java
@@ -2,87 +2,41 @@ package khmtk60.miniprojects.G6;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 
 import com.google.gson.JsonIOException;
 
-import khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model.MinMaxTypeMultiKnapsackInput;
 import khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model.MinMaxTypeMultiKnapsackInputBin;
 import khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model.MinMaxTypeMultiKnapsackInputItem;
 
-public class GeneticAlgorithm {
-	int num_items;
-	int num_bins;
-	int num_types = 0;
-	int num_classes = 0;
-	double[] w, p; // trọng số 1, 2
-	int[] t, r; // thể loại lớp
-	int[][] D; // Các bin mà item có thể thêm vào
-	double[] W, LW, P; // Max và min trọng số 1, max trọng số 2
-	int[] T, R; // Max số thể loại và số lớp 
-	MinMaxTypeMultiKnapsackInput input;
+public class GeneticAlgorithm extends Solution {
+	Greedy greedy;
 
 	public GeneticAlgorithm() throws FileNotFoundException {
-		input = new MinMaxTypeMultiKnapsackInput().loadFromFile(
-				"src\\khmtk60\\miniprojects\\multiknapsackminmaxtypeconstraints\\MinMaxTypeMultiKnapsackInput-3000.json");
-		num_items = input.getItems().length;
-		num_bins = input.getBins().length;
-		w = new double[num_items];
-		p = new double[num_items];
-		t = new int[num_items];
-		r = new int[num_items];
-		D = new int[num_items][];
-
-		W = new double[num_bins];
-		LW = new double[num_bins];
-		P = new double[num_bins];
-		T = new int[num_bins];
-		R = new int[num_bins];
-
-		for (int i = 0; i < num_items; i++) {
-			w[i] = input.getItems()[i].getW();
-			p[i] = input.getItems()[i].getP();
-			t[i] = input.getItems()[i].getT();
-			if (t[i] > num_types) {
-				num_types = t[i];
-			}
-			r[i] = input.getItems()[i].getR();
-			if (r[i] > num_classes) {
-				num_classes = r[i];
-			}
-			D[i] = input.getItems()[i].getBinIndices();
-		}
-
-		for (int i = 0; i < num_bins; i++) {
-			W[i] = input.getBins()[i].getCapacity();
-			LW[i] = input.getBins()[i].getMinLoad();
-			P[i] = input.getBins()[i].getP();
-			T[i] = input.getBins()[i].getT();
-			R[i] = input.getBins()[i].getR();
-		}
+		super();
 	}
 
-	public int[] gene() {
-		Random r = new Random();
+	public int[] gene() throws JsonIOException, IOException {
 		int[] X = new int[num_items];
-		for (int i = 0; i < num_items; i++) {
-//			int roll = r.nextInt(D[i].length + 1);
-//			if(roll == D[i].length) {
+//		Random r = new Random();
+//		for (int i = 0; i < num_items; i++) {
+//			int roll = r.nextInt(100);
+//			if (roll < 10) {
 //				X[i] = -1;
 //			} else {
-//				X[i] = D[i][roll];
+//				X[i] = D[i][r.nextInt(D[i].length)];
 //			}
-			int roll = r.nextInt(100);
-			if (roll < 10) {
-				X[i] = -1;
-			} else {
-				X[i] = D[i][r.nextInt(D[i].length)];
-			}
+//
+//		}
+//		return X;
+		for(int i = 0; i < num_items; i++) {
+			X[i] = i;
 		}
-		return X;
+		X = Utilities.arrayShuffle(X);
+		int[] sol = firstFit(X);
+		return sol;
 	}
 
 	/**
@@ -90,7 +44,79 @@ public class GeneticAlgorithm {
 	 * @param gene
 	 * @return giá trị fitness, càng cao càng tốt
 	 */
-	public float fitness(int[] gene) {
+	public int fitness(int[] gene) {
+//		int nbBins = input.getBins().length;
+//		double[] loadWeight = new double[nbBins];
+//		double[] loadP = new double[nbBins];
+//		HashSet<Integer>[] loadType = new HashSet[nbBins];
+//		HashSet<Integer>[] loadClass = new HashSet[nbBins];
+//
+//		for (int b = 0; b < nbBins; b++) {
+//			loadWeight[b] = 0;
+//			loadP[b] = 0;
+//			loadType[b] = new HashSet<Integer>();
+//			loadClass[b] = new HashSet<Integer>();
+//		}
+//		int nbItemNotScheduled = 0;
+//		float violations = 0;
+//		int[] X = gene;
+//
+//		MinMaxTypeMultiKnapsackInputItem[] items = input.getItems();
+//		MinMaxTypeMultiKnapsackInputBin[] bins = input.getBins();
+//
+//		for (int i = 0; i < X.length; i++) {
+//			if (X[i] < 0 || X[i] >= nbBins) {
+//				nbItemNotScheduled++;
+//			} else {
+//				int b = X[i];
+//				loadWeight[b] += items[i].getW();
+//				loadP[b] += items[i].getP();
+//				loadType[b].add(items[i].getT());
+//				loadClass[b].add(items[i].getR());
+//
+//				boolean ok = false;
+//				for (int j = 0; j < items[i].getBinIndices().length; j++) {
+//					if (b == items[i].getBinIndices()[j]) {
+//						ok = true;
+//						break;
+//					}
+//				}
+//				if (!ok) {
+//					violations++;
+//				}
+//			}
+//		}
+//		for (int b = 0; b < nbBins; b++) {
+//			if (loadWeight[b] > 0 && (loadWeight[b] > bins[b].getCapacity())) {
+//				violations += (loadWeight[b] - bins[b].getCapacity()) / 10;
+//			} else if (loadWeight[b] > 0 && loadWeight[b] < bins[b].getMinLoad()) {
+//				violations += (bins[b].getMinLoad() - loadWeight[b]) / 10;
+//			}
+//			if (loadP[b] > bins[b].getP()) {
+//				violations++;
+//			}
+//			if (loadType[b].size() > bins[b].getT()) {
+//				violations++;
+//			}
+//			if (loadClass[b].size() > bins[b].getR()) {
+//				violations++;
+//			}
+//		}
+//		return 20000 - (violations + nbItemNotScheduled);
+		return getLoadedItem(gene);
+	}
+
+	public int getLoadedItem(int[] solution) {
+		int loadedItem = solution.length;
+		int[] filtered = filterViolatedItem(solution);
+		for (int i : filtered) {
+			if (i == -1)
+				loadedItem--;
+		}
+		return loadedItem;
+	}
+
+	public int[] filterViolatedItem(int[] solution) {
 		int nbBins = input.getBins().length;
 		double[] loadWeight = new double[nbBins];
 		double[] loadP = new double[nbBins];
@@ -104,53 +130,41 @@ public class GeneticAlgorithm {
 			loadClass[b] = new HashSet<Integer>();
 		}
 		int nbItemNotScheduled = 0;
-		float violations = 0;
-		int[] X = gene;
+		int[] X = solution.clone();
 
 		MinMaxTypeMultiKnapsackInputItem[] items = input.getItems();
 		MinMaxTypeMultiKnapsackInputBin[] bins = input.getBins();
 
 		for (int i = 0; i < X.length; i++) {
-			if (X[i] < 0 || X[i] >= nbBins) {
-				nbItemNotScheduled++;
-			} else {
+			if (X[i] != -1) {
 				int b = X[i];
 				loadWeight[b] += items[i].getW();
 				loadP[b] += items[i].getP();
 				loadType[b].add(items[i].getT());
 				loadClass[b].add(items[i].getR());
-
-				boolean ok = false;
-				for (int j = 0; j < items[i].getBinIndices().length; j++) {
-					if (b == items[i].getBinIndices()[j]) {
-						ok = true;
-						break;
-					}
-				}
-				if (!ok) {
-					violations++;
-				}
 			}
 		}
-		for (int b = 0; b < nbBins; b++) {
+		for (int b = 0; b < nbBins; b++) { // if bin b violates one of the below, pull all items out of it
+			boolean violated = false;
 			if (loadWeight[b] > 0 && (loadWeight[b] > bins[b].getCapacity())) {
-				violations += (loadWeight[b] - bins[b].getCapacity()) / 10;
+				violated = true;
+			} else if (loadWeight[b] > 0 && loadWeight[b] < bins[b].getMinLoad()) {
+				violated = true;
+			} else if (loadP[b] > bins[b].getP()) {
+				violated = true;
+			} else if (loadType[b].size() > bins[b].getT()) {
+				violated = true;
+			} else if (loadClass[b].size() > bins[b].getR()) {
+				violated = true;
 			}
-			else if (loadWeight[b] > 0 && loadWeight[b] < bins[b].getMinLoad()) {
-				violations += (bins[b].getMinLoad() - loadWeight[b]) / 10;
-			}
-			if (loadP[b] > bins[b].getP()) {
-				violations++;
-			}
-			if (loadType[b].size() > bins[b].getT()) {
-				violations++;
-			}
-			if (loadClass[b].size() > bins[b].getR()) {
-				violations++;
+			if (violated == true) { // empty bin b
+				for (int i = 0; i < num_items; i++) {
+					if (X[i] == b)
+						X[i] = -1;
+				}
 			}
 		}
-		return 100000 - (violations + nbItemNotScheduled);
-
+		return X;
 	}
 
 	/**
@@ -187,20 +201,26 @@ public class GeneticAlgorithm {
 		int leng = gene.length;
 		int[] bestGen = Arrays.copyOf(gene, leng);
 		float bestFitness = fitness(gene);
-		int[] id = r.ints(2, 0, leng).toArray();
+		int[] id = r.ints(r.nextInt(2), 0, leng).toArray();
 		for (int i : id) {
-			for (int j = 0; j <= D[i].length; j++) {
-				if (j != D[i].length) {
-					gene[i] = D[i][j];
-					if (fitness(gene) > bestFitness) {
-						bestGen[i] = gene[i];
-					}
-				} else {
-					gene[i] = -1;
-					if (fitness(gene) > bestFitness) {
-						bestGen[i] = -1;
-					}
-				}
+//			for (int j = 0; j <= D[i].length; j++) {
+//				if (j != D[i].length) {
+//					gene[i] = D[i][j];
+//					if (fitness(gene) > bestFitness) {
+//						bestGen[i] = gene[i];
+//					}
+//				} else {
+//					gene[i] = -1;
+//					if (fitness(gene) > bestFitness) {
+//						bestGen[i] = -1;
+//					}
+//				}
+//			}
+			int roll = r.nextInt(100);
+			if (roll < 10) {
+				bestGen[i] = -1;
+			} else {
+				bestGen[i] = D[i][r.nextInt(D[i].length)];
 			}
 		}
 		return bestGen;
@@ -214,72 +234,91 @@ public class GeneticAlgorithm {
 	 * @param mutationRate tỉ lệ đột biến
 	 */
 	public void newGeneration(int[][] pop, int elitismSize, float mutationRate) {
-		int[] breedPool = new int[pop.length - elitismSize];
-		float[] roulette = new float[elitismSize];
+		int popSize = pop.length;
+		int[] breedPool = new int[popSize - elitismSize];
+		float[] roulette = new float[popSize];
 		roulette[0] = fitness(pop[0]);
-		for (int i = 1; i < elitismSize; i++) {
-			roulette[i] = roulette[i - 1] + fitness(pop[i]);
+		for (int i = 1; i < popSize; i++) {
+			float score = fitness(pop[i]);
+			if(score > 0)
+				roulette[i] = roulette[i - 1] + score;
+			else roulette[i] = roulette[i - 1] + 1;
 		}
 		Random r = new Random();
-		for (int i = 0; i < pop.length - elitismSize; i++) {
-			float val = r.nextFloat() * roulette[elitismSize - 1];
-			for (int j = 0; j < elitismSize; j++) {
-				if (val <= roulette[j]) {
+//		for (int i = 0; i < popSize - elitismSize; i++) {
+//			float val = r.nextFloat() * roulette[popSize - 1];
+//			for (int j = 0; j < popSize; j++) {
+//				if (val <= roulette[j]) {
+//					breedPool[i] = j;
+//					break;
+//				}
+//			}
+//		}
+		double[] vals = r.doubles(popSize - elitismSize, 0, roulette[popSize - 1]).toArray();
+		for(int i = 0; i < popSize - elitismSize; i++) {
+			for(int j = 0; j < popSize; j++) {
+				if(vals[i] <= roulette[j]) {
 					breedPool[i] = j;
 					break;
 				}
 			}
 		}
-		for (int i = elitismSize; i < pop.length; i = i + 2) {
-			int[][] offspring = breed(pop[breedPool[i - elitismSize]], pop[breedPool[i + 1 - elitismSize]]);
+
+		int[][] newGen = new int[popSize - elitismSize][];
+		for (int i = 0; i < popSize - elitismSize; i = i + 2) {
+			int[][] offspring = breed(pop[breedPool[i]], pop[breedPool[i + 1]]);
 			if (r.nextFloat() <= mutationRate) {
-				pop[i] = mutation(offspring[0]);
+				newGen[i] = mutation(offspring[0]);
 			} else {
-				pop[i] = offspring[0];
+				newGen[i] = offspring[0];
 			}
 			if (r.nextFloat() <= mutationRate) {
-				pop[i + 1] = mutation(offspring[1]);
+				newGen[i + 1] = mutation(offspring[1]);
 			} else {
-				pop[i + 1] = offspring[1];
+				newGen[i + 1] = offspring[1];
 			}
+		}
+		// Giu lai @elitismSize ca the tot nhat, thay toan bo so con lai bang cac ca the moi duoc sinh ra
+		for(int i = 0; i < newGen.length; i++) {
+			pop[i + elitismSize] = newGen[i];
 		}
 	}
 
-	public void printJson(int[] X) throws FileNotFoundException {
-		PrintStream fileOut = new PrintStream("outputGA.json");
-		System.setOut(fileOut);
-		System.out.print("{\"binOfItem\":[");
-		for (int i = 0; i < num_items; i++) {
-			System.out.print(X[i]);
-			if (i != num_items - 1)
-				System.out.print(",");
-		}
-		System.out.print("]}");
-	}
-
-	public void solve(int generation, int populationSize) {
+	public void solve(int generation, int populationSize) throws JsonIOException, IOException {
 		int[][] pop = new int[populationSize][];
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < populationSize; i++) {
 			pop[i] = gene();
 		}
 		Arrays.sort(pop, (a, b) -> Float.compare(fitness(b), fitness(a))); // sort pop in descending order
 		int[] bestGene = pop[0];
+		for(int j = 0; j < populationSize; j++) {
+			System.out.print(fitness(pop[j]) + " ");
+		}
+		System.out.println();
 		System.out.println("Breeding");
-		for (int i = 0; i < 100; i++) {
-			newGeneration(pop, 70, 0.05f);
+		for (int i = 0; i < generation; i++) {
+			newGeneration(pop, 20, 0.1f);
 			Arrays.sort(pop, (a, b) -> Float.compare(fitness(b), fitness(a))); // sort pop in descending order
 			bestGene = pop[0];
-			System.out.println("Best population " + fitness(bestGene) + " at generation " + i);
-		}
+			System.out.println("Population best: " + getLoadedItem(bestGene) + " item at generation " + i);
+			for(int j = 0; j < populationSize; j++) {
+				System.out.print(fitness(pop[j]) + " ");
+				if(j == 19)
+					System.out.print("| ");
+			}
+			System.out.println();
+		}		
+		int[] bestFilteredSol = filterViolatedItem(bestGene);
 		try {
-			printJson(bestGene);
+			Utilities.printJson(bestFilteredSol, "outputGA2.json");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) throws JsonIOException, IOException {
+	public static void main(String[] args) throws IOException {
 		GeneticAlgorithm hoa = new GeneticAlgorithm();
-		hoa.solve(50, 100);
+		hoa.solve(100, 100);
+
 	}
 }
--- OpenCBLS/src/khmtk60/miniprojects/G6/Greedy.java
+++ OpenCBLS/src/khmtk60/miniprojects/G6/Greedy.java
@@ -13,56 +13,12 @@ import khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model.MinMaxTypeM
 import khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model.MinMaxTypeMultiKnapsackInputItem;
 import khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model.MinMaxTypeMultiKnapsackSolution;
 
-public class Greedy {
-	int num_items;
-	int num_bins;
-	int num_types = 0;
-	int num_classes = 0;
-	double[] w, p; // item weight 1, weight 2
-	int[] t, r; // item type, class
-	int[][] D; // Allowed bin indices
-	double[] W, LW, P; // Maximum and minimum bin weight 1, maximum bin weight 2
-	int[] T, R; // Maximum different types and classes of bin
-	MinMaxTypeMultiKnapsackInput input;
-
-	private void init() throws FileNotFoundException {
-		input = new MinMaxTypeMultiKnapsackInput().loadFromFile(
-				"src\\khmtk60\\miniprojects\\multiknapsackminmaxtypeconstraints\\MinMaxTypeMultiKnapsackInput-3000.json");
-		num_items = input.getItems().length;
-		num_bins = input.getBins().length;
-		w = new double[num_items];
-		p = new double[num_items];
-		t = new int[num_items];
-		r = new int[num_items];
-		D = new int[num_items][];
-
-		W = new double[num_bins];
-		LW = new double[num_bins];
-		P = new double[num_bins];
-		T = new int[num_bins];
-		R = new int[num_bins];
-
-		for (int i = 0; i < num_items; i++) {
-			w[i] = input.getItems()[i].getW();
-			p[i] = input.getItems()[i].getP();
-			t[i] = input.getItems()[i].getT();
-			if (t[i] > num_types) {
-				num_types = t[i];
-			}
-			r[i] = input.getItems()[i].getR();
-			if (r[i] > num_classes) {
-				num_classes = r[i];
-			}
-			D[i] = input.getItems()[i].getBinIndices();
-		}
-
-		for (int i = 0; i < num_bins; i++) {
-			W[i] = input.getBins()[i].getCapacity();
-			LW[i] = input.getBins()[i].getMinLoad();
-			P[i] = input.getBins()[i].getP();
-			T[i] = input.getBins()[i].getT();
-			R[i] = input.getBins()[i].getR();
-		}
+public class Greedy extends Solution{
+	
+	
+	public Greedy() throws FileNotFoundException{
+		// TODO Auto-generated constructor stub
+		super();
 	}
 
 	/**
@@ -71,7 +27,7 @@ public class Greedy {
 	 * @throws JsonIOException
 	 * @throws IOException
 	 */
-	private int[] firstFit() throws JsonIOException, IOException {
+	public int[] firstFit() throws JsonIOException, IOException {
 		int X[] = new int[num_items];
 		double bW[] = new double[num_bins]; // bin weight 1
 		double bP[] = new double[num_bins]; // bin weight 2
@@ -104,7 +60,7 @@ public class Greedy {
 							continue;
 						}
 					}
-					System.out.println("Item " + i + " is added to bin " + j);
+//					System.out.println("Item " + i + " is added to bin " + j);
 					X[i] = j;
 					bW[j] += w[i];
 					bP[j] += p[i];
@@ -113,7 +69,7 @@ public class Greedy {
 					continue itemLoop;
 				}
 			}
-			System.out.println("Item " + i + " cannot be added to any bins");
+//			System.out.println("Item " + i + " cannot be added to any bins");
 			X[i] = -1;
 		}
 //		for(int i = 0; i < num_bins; i++) {
@@ -125,9 +81,11 @@ public class Greedy {
 				unallocated_item += 1;
 			}
 		}
-		System.out.println("There are " + unallocated_item + " item(s) not added to any bins");
+//		System.out.println("There are " + unallocated_item + " item(s) not added to any bins");
 		return X;
 	}
+
+
 
 	public float fitness(int[] solution) {
 		int nbBins = input.getBins().length;
@@ -274,26 +232,15 @@ public class Greedy {
 			X[i] = bestBin;
 			System.out.println("X[" + i + "] = " + bestBin);
 		}
-		printJson(X);
+		Utilities.printJson(X, "outputGreedy.json");
 	}
 
-	public void printJson(int[] X) throws FileNotFoundException {
-		PrintStream fileOut = new PrintStream("output.json");
-		System.setOut(fileOut);
-		System.out.print("{\"binOfItem\":[");
-		for (int i = 0; i < num_items; i++) {
-			System.out.print(X[i]);
-			if (i != num_items - 1)
-				System.out.print(",");
-		}
-		System.out.print("]}");
-	}
+	
 
 	public static void main(String[] args) throws JsonIOException, IOException {
 		Greedy hoa = new Greedy();
-		hoa.init();
-		int[] sol = hoa.firstFit();
-		hoa.greedy(sol);
-		hoa.printJson(sol);
+		GeneticAlgorithm ga = new GeneticAlgorithm();
+		int[] sol;
+		sol = hoa.firstFit();
 	}
 }
