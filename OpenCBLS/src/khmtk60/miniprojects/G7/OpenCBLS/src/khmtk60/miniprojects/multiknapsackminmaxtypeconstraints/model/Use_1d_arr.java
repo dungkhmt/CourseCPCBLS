b089c918421a324bb886bdc6420ec68419f4a135
theKuam <ngominh.hai96@gmail.com>
2019-05-09 12:58:39 +0700

    add code group G7

---
+++ OpenCBLS/src/khmtk60/miniprojects/G7/OpenCBLS/src/khmtk60/miniprojects/multiknapsackminmaxtypeconstraints/model/Use_1d_arr.java
@@ -0,0 +1,385 @@
+package khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model;
+
+import java.util.*;
+
+import khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model.MinMaxTypeMultiKnapsackSolution.AssignMove;
+
+public class Use_1d_arr {
+	MinMaxTypeMultiKnapsackInputBin[] bins;
+	MinMaxTypeMultiKnapsackInputItem[] items;
+	double capacity[];
+	double minLoad[];
+	double p_bin[];
+	int t_bin[];
+	int r_bin[];
+	double w[];
+	double p_item[];
+	int t_item[];
+	int r_item[];
+	int [][] binIndices;
+	int int_w[];
+	int int_capacity[];
+	int int_p_item[];
+	int int_p_bin[];
+	int int_minload[];
+	int boiso = 1000;
+	int n;
+	public Use_1d_arr(MinMaxTypeMultiKnapsackInputBin[] bins, MinMaxTypeMultiKnapsackInputItem[] items,
+			double[] capacity, double[] minLoad, double[] p_bin, int[] t_bin, int[] r_bin, double[] w, double[] p_item,
+			int[] t_item, int[] r_item, int[][] binIndices) {
+		super();
+		this.bins = bins;
+		this.items = items;
+		this.capacity = capacity;
+		this.minLoad = minLoad;
+		this.p_bin = p_bin;
+		this.t_bin = t_bin;
+		this.r_bin = r_bin;
+		this.w = w;
+		this.p_item = p_item;
+		this.t_item = t_item;
+		this.r_item = r_item;
+		this.binIndices = binIndices;
+	}
+	int m;
+	int mt;
+	int mr;
+	
+	public int sum(int []a) {
+		int sum = 0;
+		for(int i=0;i<a.length;i++) {
+			sum = sum + a[i];
+		}
+		return sum;
+	}
+ 
+	public int dem(int[]X, int []a, int i) {
+		int b[] = new int[count(a)];
+		int count = 0;
+		for(int j=0;j<w.length;j++) {
+			if(X[j] == i) {
+				b[a[j]] = 1;
+			}
+		}
+		count = sum(b);
+		return count;
+	}
+	
+	
+
+	public void convert_data_from_double_to_int() {
+		int_w = new int[w.length];
+		int_capacity = new int [capacity.length];
+		int_minload = new int [minLoad.length];
+		int_p_item = new int [p_item.length];
+		int_p_bin = new int [p_bin.length];
+        for(int i=0;i<w.length;i++) {
+        	int_w[i] = (int) (w[i]*boiso);
+        	int_p_item[i] = (int) (p_item[i]*boiso);
+        }      
+        
+        for(int i=0;i<p_bin.length;i++) {
+        	int_p_bin[i] = (int)(p_bin[i]*boiso);
+        	int_capacity[i] = (int) (capacity[i]*boiso);
+        	int_minload[i] = (int) (minLoad[i]*boiso);
+        }
+	}
+	
+	public int count (int []arr) {
+		HashSet<Integer> hashSet = new HashSet<>();
+		for(int i=0;i<arr.length;i++) {
+			hashSet.add(arr[i]);
+		}
+		return hashSet.size();
+	}
+	public void gan_mien_gia_tri() {
+		n = items.length;
+		m = bins.length;
+		mt = count(t_item);
+		mr = count(r_item);
+	}
+	public double[] tinh_wb_items(int []X) {
+		double[]wb = new double[m];
+		for(int i=0;i<n;i++) {
+			wb[X[i]] = wb[X[i]] + w[i];
+		}
+		
+		return wb;
+	}
+	public double[] tinh_pb_items(int []X) {
+		double []pb = new double[m];		
+		for(int i=0;i<n;i++) {
+			pb[X[i]] = pb[X[i]] + p_item[i];
+		}
+		return pb;
+	}
+	int new_t_item[];
+	int new_r_item[];
+	
+	
+	/*ArrayList <Double> luu_gt = new A
+	class bien_luu_gia_tri{
+		double violations;
+		
+	}*/
+	double wb[];
+	double pb[];
+	int dem_so_type;
+	int dem_so_class;
+	public double violations(int[]X) {
+//		double[] wb = new double[m];
+//		wb = tinh_wb_items(X);
+//		double[] pb = new double[m];
+//		pb = tinh_pb_items(X);
+//		double violation = 0;
+//		for(int i=0;i<bins.length;i++) {
+//			violation += Math.max(0,wb[i]-capacity[i])+ Math.max(0, minLoad[i]-wb[i])+Math.max(0, pb[i]-p_bin[i])
+//			+Math.max(0, dem(X,new_t_item,i)-t_bin[i]) + Math.max(0, dem(X,new_r_item,i) - r_bin[i]);
+//		}
+//		return violation;
+		
+		wb = new double[m];
+		wb = tinh_wb_items(X);
+		pb = new double[m];
+		pb = tinh_pb_items(X);
+		double violation = 0;
+		for(int i=0;i<bins.length;i++) {
+			int tBinCount = dem(X,new_t_item,i);
+			int rBinCount = dem(X,new_r_item,i);
+			violation += Math.max(0,wb[i]-capacity[i])+ Math.max(0, minLoad[i]-wb[i])+Math.max(0, pb[i]-p_bin[i])
+					+Math.max(0, tBinCount-t_bin[i]) + Math.max(0, rBinCount - r_bin[i]);
+
+		}
+		return violation;
+		
+	}
+	public double getAssignDelta(int[]X, int index, int bin_moi) {
+		double [] wb1;
+		double [] pb1;
+		int bin_cu = X[index];
+		/*wb1 = tinh_wb_items(X);	
+		pb1 = tinh_pb_items(X);
+		double tong_trong_so_bin_cu_truoc_khi_assign = Math.max(0,wb1[bin_cu]-capacity[bin_cu])
+				+ Math.max(0, minLoad[bin_cu]-wb1[bin_cu])
+				+Math.max(0, pb1[bin_cu]-p_bin[bin_cu])
+				+Math.max(0, dem(X,new_t_item,bin_cu)-t_bin[bin_cu]) 
+				+ Math.max(0, dem(X,new_r_item,bin_cu) - r_bin[bin_cu]);
+		double tong_trong_so_bin_moi_truoc_khi_assign = Math.max(0,wb1[bin_moi]-capacity[bin_moi])
+				+ Math.max(0, minLoad[bin_moi]-wb1[bin_moi])
+				+Math.max(0, pb1[bin_moi]-p_bin[bin_moi])
+				+Math.max(0, dem(X,new_t_item,bin_moi)-t_bin[bin_moi]) 
+				+ Math.max(0, dem(X,new_r_item,bin_moi) - r_bin[bin_moi]);*/
+		double tong_trong_so_bin_cu_truoc_khi_assign = Math.max(0, minLoad[bin_cu]-wb[bin_cu]);
+		double tong_trong_so_bin_moi_truoc_khi_assign = Math.max(0, minLoad[bin_moi]-wb[bin_moi]);
+
+
+		X[index] = bin_moi;
+
+		wb1 = tinh_wb_items(X);
+		pb1 = tinh_pb_items(X);
+		double tong_trong_so_bin_cu_sau_khi_assign = Math.max(0, minLoad[bin_cu]-wb1[bin_cu]);
+		double tong_trong_so_bin_moi_sau_khi_assign = Math.max(0, minLoad[bin_moi]-wb1[bin_moi]);
+		double vio_change = (tong_trong_so_bin_moi_sau_khi_assign + tong_trong_so_bin_cu_sau_khi_assign) - (tong_trong_so_bin_cu_truoc_khi_assign + tong_trong_so_bin_moi_truoc_khi_assign);
+		//System.out.println("vio_change: " +vio_change);
+		//System.out.println("tong_trong_so_bin_moi_truoc_khi_assign: " +tong_trong_so_bin_cu_truoc_khi_assign);
+		//System.out.println("tong_trong_so_bin_moi_sau_khi_assign: " +tong_trong_so_bin_moi_sau_khi_assign);
+		X[index] = bin_cu;
+		return vio_change;	
+		/*int bin_cu = X[index];
+		X[index] = bin_moi;
+		vio_change = violations(X);
+		X[index] = bin_cu;
+		return vio_change;*/
+	}
+	ArrayList<Integer> a  = new ArrayList<Integer>();
+	public void check(int []X) {
+		double wb[] = new double[m];
+		double pb[] = new double[m];
+		wb = tinh_wb_items(X);
+		pb = tinh_pb_items(X);
+
+		for(int i=0;i<m;i++) {
+			System.out.println(" wb= "+wb[i]+ " pb= " +pb[i]+ " so type= "+dem(X,new_r_item,i) );
+		}
+		for(int i=0;i<n;i++) {
+			System.out.print(X[i] +" ");
+		}
+		for(int i=0;i<n;i++) {
+			if( (wb[X[i]] >= minLoad[X[i]]) && (wb[X[i]]<=capacity[X[i]]) && pb[X[i]]<=p_bin[X[i]] && (dem(X,new_t_item,X[i]) <= t_bin[X[i]]) 
+					&& (dem(X,new_r_item,X[i]) <= r_bin[X[i]]) ) {
+				
+			}else {
+				X[i] = -1;
+			}
+		}
+		System.out.println();
+		System.out.println("gia tri sau khi check: ");
+		for(int i=0;i<n;i++) {
+			System.out.print(X[i] +" ");
+		}
+	}
+	
+	class AssignMove{
+	    int i;
+	    int v;
+
+	    public AssignMove(int i, int v) {
+	        this.i = i;
+	        this.v = v;
+	    } 
+	}
+	public int[] type_or_class(int arr[]) {
+		HashSet<Integer> hashSet = new HashSet<>();
+		
+		for(int i=0;i<arr.length;i++) {
+			hashSet.add(arr[i]);
+		}
+		Integer list[] = new Integer[hashSet.size()];
+		list = hashSet.toArray(list);
+		int list_type_or_class[] = new int[list.length];
+		for (int i=0;i<list.length;i++) {
+			list_type_or_class [i]= list[i].intValue();
+		}
+		//list_type_or_class = sort(list_type_or_class);
+		int new_arr[] = new int[arr.length];
+		for(int i= 0;i<arr.length;i++) {
+			for(int j=0;j<list_type_or_class.length;j++) {
+				if(arr[i] == list_type_or_class[j]) {
+					new_arr[i] = j;
+				}
+			}
+		}
+		return new_arr;
+	}
+public void test() {
+	double pb[] = new double[m];
+	pb = tinh_pb_items(X);
+	for(int i=0;i<m;i++) {
+		System.out.println(p_item[i]);
+	}
+}
+
+	
+	int X[];
+
+	public void stateModel() {
+		X = new int[n];
+		Arrays.fill(X, -1);
+		Random rand = new Random();
+		greedyInitializeX();
+		// randomInitializeX(rand);
+		//convert_data_from_double_to_int();
+		new_t_item = new int[t_item.length];
+		new_t_item = type_or_class(t_item);
+		new_r_item = new int[r_item.length];
+		new_r_item = type_or_class(r_item);
+		//test();
+		System.out.println(violations(X));
+		int it = 0;
+		ArrayList<AssignMove> am = new ArrayList<AssignMove>();
+		while(it < 10000 && violations(X) > 0) {
+			am.clear();
+			//double vio = violations(X);
+			double min_delta = Integer.MAX_VALUE;
+			for(int i = 0; i < items.length; i++) {
+				for(int v: binIndices[i]) {
+					double delta = getAssignDelta(X, i, v);
+					//System.out.println(delta);
+					if (delta < min_delta) {
+						min_delta = delta;
+						am.clear();
+						am.add(new AssignMove(i, v));
+					} else if(delta == min_delta) {
+						if(!am.contains(new AssignMove(i, v))) {
+							am.add(new AssignMove(i, v));
+						}
+					}
+				}
+			}
+			//System.out.println(min_delta);
+			int idx = rand.nextInt(am.size());
+			//System.out.println(idx);
+			AssignMove m = am.get(idx);
+			//System.out.println("i= "+m.i + "v= "+m.v);
+			X[m.i]=m.v;
+			System.out.println("Step " + it + ", violations  = " + violations(X));
+			it++;
+		}
+		
+		
+		check(X);
+		
+		
+	}
+
+	public void greedyInitializeX() {
+		float[] current_w_bin = new float[m];
+		float[] current_p_bin = new float[m];
+		int[] current_t_bin = new int[m];
+		int[] current_r_bin = new int[m];
+
+		ArrayList<Set> tBin = new ArrayList<>();
+		ArrayList<Set> rBin = new ArrayList<>();
+
+		for (int i = 0; i < m; i++) {
+			Set<Integer> tSet = new HashSet<>();
+			Set<Integer> rSet = new HashSet<>();
+			tBin.add(tSet);
+			rBin.add(rSet);
+		}
+
+		for (int i = 0; i < n; i++) {
+			int[] binDomain = binIndices[i];
+			for (int bin : binDomain) {
+				if (current_w_bin[bin] + w[i] <= capacity[bin] && current_p_bin[bin] + p_item[i] <= p_bin[bin]) {
+					if ((current_t_bin[bin] > t_bin[bin]) || (current_r_bin[bin] > r_bin[bin])) {
+						//Do nothing
+					}
+					else if ((!rBin.get(bin).contains(r_item[i])) && (current_r_bin[bin] == r_bin[bin])) {
+						//Do nothing
+					}
+					else if ((!tBin.get(bin).contains(t_item[i])) && (current_t_bin[bin] == t_bin[bin])) {
+						//Do nothing
+					}
+					else {
+						if (!tBin.get(bin).contains(t_item[i])) {
+							X[i] = bin;
+							current_w_bin[bin] += w[i];
+							current_p_bin[bin] += p_item[i];
+							if (!rBin.get(bin).contains(r_item[i])) {
+								current_r_bin[bin] += 1;
+								rBin.get(bin).add(r_item[i]);
+								break;
+							}
+							break;
+						}
+						else {
+							X[i] = bin;
+							current_w_bin[bin] += w[i];
+							current_p_bin[bin] += p_item[i];
+							if (!rBin.get(bin).contains(r_item[i])) {
+								current_t_bin[bin] += 1;
+								tBin.get(bin).add(t_item[i]);
+								current_r_bin[bin] += 1;
+								rBin.get(bin).add(r_item[i]);
+								break;
+							}
+							break;
+
+						}
+					}
+				}
+			}
+		}
+		tBin.clear();
+		rBin.clear();
+	}
+
+	private void randomInitializeX(Random rand) {
+		for(int i=0;i<n;i++) {
+			int idx = rand.nextInt(binIndices[i].length);
+
+			X[i] = binIndices[i][idx];
+		}
+	}
+}
---
+++ OpenCBLS/src/khmtk60/miniprojects/G7/OpenCBLS/src/khmtk60/miniprojects/multiknapsackminmaxtypeconstraints/model/Use_2d_arr.java
@@ -0,0 +1,299 @@
+package khmtk60.miniprojects.multiknapsackminmaxtypeconstraints.model;
+
+import java.util.HashSet;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+
+import choco.kernel.model.constraints.cnf.ALogicTree.Type;
+import localsearch.constraints.basic.IsEqual;
+import localsearch.constraints.basic.LessOrEqual;
+import localsearch.functions.basic.FuncMult;
+import localsearch.functions.basic.FuncPlus;
+import localsearch.functions.conditionalsum.ConditionalSum;
+import localsearch.functions.sum.Sum;
+import localsearch.model.ConstraintSystem;
+import localsearch.model.IConstraint;
+import localsearch.model.IFunction;
+import localsearch.model.LocalSearchManager;
+import localsearch.model.VarIntLS;
+import localsearch.search.TabuSearch;
+public class Use_2d_arr {
+	MinMaxTypeMultiKnapsackInputBin[] bins;
+	MinMaxTypeMultiKnapsackInputItem[] items;
+	double capacity[];
+	double minLoad[];
+	double p_bin[];
+	int t_bin[];
+	int r_bin[];
+	double w[];
+	double p_item[];
+	int t_item[];
+	int r_item[];
+	int [][] binIndices;
+	int int_w[];
+	int int_capacity[];
+	int int_p_item[];
+	int int_p_bin[];
+	int int_minload[];
+	int boiso = 10000;
+	public Use_2d_arr() {
+		super();
+	}
+	
+	public Use_2d_arr(
+			MinMaxTypeMultiKnapsackInputBin[] bins, MinMaxTypeMultiKnapsackInputItem[] items, double[] capacity,
+			double[] minLoad, double[] p_bin, int[] t_bin, int[] r_bin, double[] w, double[] p_item, int[] t_item,
+			int[] r_item, int[][] binIndices) {
+		super();
+		this.bins = bins;
+		this.items = items;
+		this.capacity = capacity;
+		this.minLoad = minLoad;
+		this.p_bin = p_bin;
+		this.t_bin = t_bin;
+		this.r_bin = r_bin;
+		this.w = w;
+		this.p_item = p_item;
+		this.t_item = t_item;
+		this.r_item = r_item;
+		this.binIndices = binIndices;
+	}
+
+	public void convert_data_from_double_to_int() {
+		int_w = new int[w.length];
+		int_capacity = new int [capacity.length];
+		int_minload = new int [minLoad.length];
+		int_p_item = new int [p_item.length];
+		int_p_bin = new int [p_bin.length];
+        for(int i=0;i<w.length;i++) {
+        	int_w[i] = (int) (w[i]*boiso);
+        	int_p_item[i] = (int) (p_item[i]*boiso);
+        }      
+        
+        for(int i=0;i<p_bin.length;i++) {
+        	int_p_bin[i] = (int)(p_bin[i]*boiso);
+        	int_capacity[i] = (int) (capacity[i]*boiso);
+        	int_minload[i] = (int) (minLoad[i]*boiso);
+        }
+	}
+	public int count (int []arr) {
+		HashSet<Integer> hashSet = new HashSet<>();
+		for(int i=0;i<arr.length;i++) {
+			hashSet.add(arr[i]);
+		}
+		return hashSet.size();
+	}
+	public int[] sort(int[]arr) {
+		int temp = arr[0];
+        for (int i = 0 ; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        return arr;
+	}
+	public int[] type_or_class(int arr[]) {
+		HashSet<Integer> hashSet = new HashSet<>();
+		
+		for(int i=0;i<arr.length;i++) {
+			hashSet.add(arr[i]);
+		}
+		Integer list[] = new Integer[hashSet.size()];
+		list = hashSet.toArray(list);
+		int list_type_or_class[] = new int[list.length];
+		for (int i=0;i<list.length;i++) {
+			list_type_or_class [i]= list[i].intValue();
+		}
+		//list_type_or_class = sort(list_type_or_class);
+		int new_arr[] = new int[arr.length];
+		for(int i= 0;i<arr.length;i++) {
+			for(int j=0;j<list_type_or_class.length;j++) {
+				if(arr[i] == list_type_or_class[j]) {
+					new_arr[i] = j;
+				}
+			}
+		}
+		return new_arr;
+	}
+	LocalSearchManager mgr;
+	ConstraintSystem S;
+	private VarIntLS[][] x;
+    private VarIntLS[][] Y;
+    private VarIntLS[][] Z;
+    int n;
+    int m;
+    int mt;
+    int mr;
+    class AssignMove{
+	    int i;
+	    int v;
+
+	    public AssignMove(int i, int v) {
+	        this.i = i;
+	        this.v = v;
+	    } 
+	}
+    public void HillClimbing(IConstraint c, int maxIter) {
+        VarIntLS[] y = c.getVariables();
+        ArrayList<AssignMove> cand = new ArrayList<AssignMove>();
+        int it = 0;
+        Random R = new Random();
+        while(it < maxIter && c.violations() > 0) {
+            cand.clear();
+            float minDelta = Integer.MAX_VALUE;
+            for(int i = 0 ; i < y.length ; i++) {
+                for( int v = y[i].getMinValue(); v <= y[i].getMaxValue(); v++) {
+                    float d = c.getAssignDelta(y[i], v);
+                    if(d < minDelta ) {
+                        cand.clear();
+                        cand.add(new AssignMove(i, v));
+                        minDelta = d;
+                    }
+                    else if(d == minDelta) {
+                        cand.add(new AssignMove(i, v));
+                    }
+                }
+            }
+            int idx= R.nextInt(cand.size());
+            AssignMove m  = cand.get(idx);
+            y[m.i].setValuePropagate(m.v);;
+            System.out.println("Step " +it + " violations = " + c.violations());
+            it++;
+        }
+    }
+    public void search() {
+		//TabuSearch ts = new TabuSearch();
+        //ts.search(S, 50, 50, 100000, 100);
+		//System.out.println("ket thuc");
+    	
+		System.out.println(S.violations());
+		HillClimbing(S, 10000);
+		inKQ(x);
+		//HillClimbingSearch();
+
+	}
+	public void inKQ(VarIntLS X[][]) {
+		int[] KQ = new int[bins.length];
+		for(int i=0;i<items.length;i++) {
+			for(int j=0;j<bins.length;j++) {
+				if(X[i][j].getValue() == 1) {
+					KQ[i] = j;
+				}
+			}
+		}
+		System.out.println("Danh sach item thu duoc la: ");
+		for(int i=0;i<items.length;i++) {
+			System.out.print(KQ[i] + " ");
+		}
+	}
+   
+
+	public void stateModel() {
+		 mgr = new LocalSearchManager();
+		 S = new ConstraintSystem(mgr);
+		 int n = items.length;
+		 int m = bins.length;
+		 int mt = count(t_item);
+		 int mr = count(r_item);
+	     x = new VarIntLS[n][m];
+	     Y = new VarIntLS[mt][m];
+	     Z = new VarIntLS[mr][m];
+	     Random rd = new Random();
+	     for (int i = 0 ; i < n; i++){
+	            for (int j = 0; j < m; j++) {
+	                x[i][j] = new VarIntLS(mgr, 0, 1);
+	                //x[i][j].setValue(rd.nextInt(2));
+
+	            }
+	        }
+	        //init Y
+	        for (int i = 0 ; i < mt; i++){
+	            for (int j= 0; j < m; j++) {
+	                Y[i][j] = new VarIntLS(mgr, 0 , 1);
+	                //Y[i][j].setValue(rd.nextInt(2));
+	            }
+	        }
+	        //init Z
+	        for (int i = 0 ; i < mr; i++){
+	            for (int j = 0; j < m; j++) {
+	                Z[i][j] = new VarIntLS(mgr,0,1);
+	                //Z[i][j].setValue(rd.nextInt(2));
+	            }
+	        }
+	        
+	        for (int i = 0; i < n; i++){
+	            S.post(new IsEqual(new Sum(x[i]), 1));
+	            IFunction[] f = new IFunction[binIndices[i].length];
+	            for(int j=0 ;j<binIndices[i].length;j++) {
+	            	f[j] = new FuncPlus( x[i][binIndices[i][j]],0);
+	            	
+	            }
+	            S.post(new LessOrEqual(new Sum(f), 1));
+	            //S.post(new IsEqual(new FuncPlus(x[i][binIndices[i][0]], x[i][binIndices[i][1]]), 1));
+	            //S.post(new LessOrEqual(1, new FuncPlus(x[i][D[i][0]], x[i][D[i][1]])));
+	        }
+	        
+	        convert_data_from_double_to_int();
+	        
+	        IFunction[] f1 = new IFunction[n];
+	        for (int j =0; j < m; j++){
+	            for(int i = 0; i < n; i++){
+	                f1[i] =  new FuncMult(x[i][j], int_w[i]);
+	            }
+	            S.post(new LessOrEqual(new Sum(f1), int_capacity[j]));
+	            S.post(new LessOrEqual((int) (minLoad[j]*boiso)	, new Sum(f1)));
+	        }
+
+	        IFunction[] f2 = new IFunction[n];
+	        for (int j =0; j < m; j++){
+	            for(int i = 0; i < n; i++){
+	                f2[i] = new FuncMult(x[i][j], (int)(p_item[i]*boiso));
+	            }
+	            S.post(new LessOrEqual(new Sum(f2), (int) (p_bin[j]*boiso)));
+	        }
+	        
+	        
+	        for (int j= 0; j < m; j++){
+	            VarIntLS[] t = new VarIntLS[mt];
+	            for (int i =0; i < mt; i++){
+	                t[i]= Y[i][j];
+	            }
+	            S.post(new LessOrEqual(new Sum(t), t_bin[j]));
+	        }
+
+	        for (int j= 0; j < m; j++){
+	            VarIntLS[] r = new VarIntLS[mr];
+	            for (int i =0; i < mr; i++){
+	                r[i]= Z[i][j];
+	            }
+	            S.post(new LessOrEqual(new Sum(r), r_bin[j]));
+	        }
+
+	        
+	        t_item = type_or_class(t_item);
+	        r_item = type_or_class(r_item);
+
+	        for(int i = 0; i < n; i++){
+	            for (int j = 0; j < m; j++){
+	                S.post(new LessOrEqual(x[i][j],Y[t_item[i]][j]));
+	            }
+	        }
+
+	        for(int i = 0; i < n; i++){
+	            for (int j = 0; j <m; j++){
+	                S.post(new LessOrEqual(x[i][j],Z[r_item[i]][j]));
+	            }
+	        }
+	        mgr.close();
+	        
+	}
+	
+}
