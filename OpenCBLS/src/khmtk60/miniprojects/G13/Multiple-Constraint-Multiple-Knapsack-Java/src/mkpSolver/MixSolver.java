b7fba2a4cdb885594cc0fbbbea76ad3950a5fb57
Lionel <lionel@cinnamon.is>
2019-05-10 10:42:24 +0700

    refactor code

--- OpenCBLS/src/khmtk60/miniprojects/G13/Multiple-Constraint-Multiple-Knapsack-Java/src/mkpSolver/MixSolver.java
+++ OpenCBLS/src/khmtk60/miniprojects/G13/Multiple-Constraint-Multiple-Knapsack-Java/src/mkpSolver/MixSolver.java
@@ -2,75 +2,6 @@ package mkpSolver;
 
 import java.util.*;
 
-class binLowWeightNeeded
-{
-	int index;
-	double low_w_needed;
-	int T = -1; //T = 0 thi khong tang T
-	int R = -1; //R = 0 thi khong tang R
-	public binLowWeightNeeded(int index, double low_w_needed)
-	{
-		this.index = index;
-		this.low_w_needed = low_w_needed;
-	}
-	
-	public binLowWeightNeeded(int index, double low_w_needed, int T, int R)
-	{
-		this.index = index;
-		this.low_w_needed = low_w_needed;
-		this.T = T;
-		this.R = R;
-	} 
-	
-	
-}
-
-class lowWComparator implements Comparator<binLowWeightNeeded> {
-	public int compare(binLowWeightNeeded a, binLowWeightNeeded b) {
-//		if(a.low_w_needed < 0) return -1;
-		if(a.low_w_needed > b.low_w_needed) //Trong so cac bin co the xep, chon bin nao?
-			return 1;
-		return -1;
-	}
-}
-
-class TRcomparator implements Comparator<binLowWeightNeeded> {
-	public int compare(binLowWeightNeeded a, binLowWeightNeeded b) {
-		if(a.R > b.R) //Trong so cac bin co the xep, chon bin nao?
-			return 1;
-		if(a.T > b.T)
-			return 1;
-		if(a.low_w_needed > b.low_w_needed) //Trong so cac bin co the xep, chon bin nao?
-			return 1;
-		return -1;
-	}
-}
-
-class Move
-{
-	int index;
-	int bin;
-	public Move(int index, int new_bin)
-	{
-		this.index = index;
-		this.bin = new_bin;
-	}
-}
-
-class A
-{
-	private Integer[] items;
-	public A(Integer[] items)
-	{
-		super();
-		this.items = items;
-//		this.bins = bins;
-	}
-	public Integer[] getItems() {
-		return items;
-	}
-}
-
 public class MixSolver {
 	//Data
 	MinMaxTypeMultiKnapsackInput inputData;
@@ -81,15 +12,19 @@ public class MixSolver {
 	Map<Integer, Set<Integer>> map_item_type;
 	Map<Integer, Set<Integer>> map_item_class;
 	//Decision variable
-	public int[] X;
+	public int[] X; // Thay doi sau moi vong lap
 	//Variable need for constraint
-	double[] low_w_needed;
+	double[] low_w_needed; //Thay doi lien tuc trong qua trinh tim kiem
+	Map<Integer, Float> current_violation_bin; //Luu lai cac bin dang vi pham va low_w_need cua no
+	Map<Integer, Integer> current_violation_item; //Cac item dang vi pham va vi tri bin cua no
 	double[] current_w;
 	double[] current_p;
 	int[] current_type;
 	int[] current_class;
 	int[][] current_bin_type;
 	int[][] current_bin_class;
+	int current_num_item;
+	double violation; //Sum of all low_w_need
 	//Violation
 	List<Integer> violation_bin;
 	List<Integer> violation_item;
@@ -124,6 +59,9 @@ public class MixSolver {
 		this.current_class = new int[this.n_bins];
 		this.current_bin_type = new int[this.n_bins][this.max_num_type+1];
 		this.current_bin_class = new int[this.n_bins][this.max_num_class+1];
+		this.current_num_item = 0;
+		this.current_violation_bin = new HashMap<Integer, Float>();
+		this.current_violation_item = new HashMap<Integer, Integer>();
 	}
 	
 	public void processData()
@@ -159,6 +97,22 @@ public class MixSolver {
 				list_item.add(Integer.valueOf(i));
 			}
 		}
+	}
+	
+	public void neighborsSearch()
+	{
+		
+	}
+	
+	public double getAssignDelta(int item_index, int new_bin_index, int old_bin_index)
+	{
+		MinMaxTypeMultiKnapsackInputItem item = this.items[item_index];
+		MinMaxTypeMultiKnapsackInputBin old_bin = this.bins[old_bin_index];
+		MinMaxTypeMultiKnapsackInputBin new_bin = this.bins[new_bin_index];
+//		float old_bin_low_w_need = this.low_w_needed[old_bin_index] = old_bin.getMinLoad() + item.getW(); 
+		double delta = 0;
+		
+		return delta;
 	}
 	
 	public void greedy_search()
@@ -210,19 +164,6 @@ public class MixSolver {
 		this.updateViolation();
 	}
 	
-	public void replace_one_one_increase_lw()
-	{
-		for(int i = 0; i < this.n_items; i++)
-		{
-			
-		}
-	}
-
-	public void replace_one_one_all()
-	{
-		
-	}
-	
 	public void add_item_to_bin()
 	{//Item khac da xep duoc roi, bay gio chi xet cac item chua xep duoc
 		List<Integer> unpack = new ArrayList<>(this.violation_item);
@@ -254,15 +195,19 @@ public class MixSolver {
 	
 	public void hillClimbing()
 	{
+		int init_item_pack = this.n_items - this.violation_item.size();
+		int current_item_pack = init_item_pack;
+		int time_not_change = 0;
 		Comparator<binLowWeightNeeded> tr_comparator = new TRcomparator();
 		PriorityQueue<binLowWeightNeeded> list_move_heuristic = new PriorityQueue<binLowWeightNeeded>(tr_comparator);
 //		List<Move> list_move_heuristic = new ArrayList<>();
 		List<Move> possible_move = new ArrayList<>();
 		Random random_move = new Random();
 		int iter = 0;
-		int max_step = 1000;
+		int max_step = 100;
 		while(iter < max_step)
 		{
+			time_not_change += 1;
 			System.out.println("Step: " + iter);
 			for(int i = 0; i < this.n_items; i++)
 			{
@@ -305,14 +250,19 @@ public class MixSolver {
 						binLowWeightNeeded new_bin = list_move_heuristic.remove();
 						int bin_index = new_bin.index;
 						X[i] = bin_index;
+						double before_low_w_need = this.low_w_needed[bin_index];
 						this.updateConstraint(bin_index, i);
+						if(before_low_w_need < 0 && this.low_w_needed[bin_index] >= 0)
+							current_item_pack++;
+							
 					}
-					else if(possible_move.size() > 0 && iter%100 == 0)
+					else if(possible_move.size() > 0 && time_not_change == 100)
 					{
 						int index = random_move.nextInt(possible_move.size());
 						Move m = possible_move.get(index);
 						X[m.index] = m.bin;
 						this.updateConstraint(m.bin, m.index);
+						time_not_change = 0;
 					}
 				}
 				else
@@ -354,13 +304,41 @@ public class MixSolver {
 							int old_bin_index = X[i];
 							X[i] = new_bin.index;
 							this.swapItem(X[i], old_bin_index, i);
-//							this.updateConstraint(bin_index, i);
+						}
+						else if(possible_move.size() > 0 && time_not_change == 100)
+						{
+							int index = random_move.nextInt(possible_move.size());
+							Move m = possible_move.get(index);
+							int old_bin_index = X[i];
+							X[m.index] = m.bin;
+							this.swapItem(X[i], old_bin_index, i);
+							time_not_change = 0;
 						}
 					}
 				}
 			}
 			iter++;
+			if(current_item_pack != init_item_pack)
+			{
+				init_item_pack = current_item_pack;
+				time_not_change = 0;
+			}
+			this.add_item_to_bin();
+			this.updateViolation();
 		}
+	}
+	
+	public void replace_one_one_increase_lw()
+	{
+		for(int i = 0; i < this.n_items; i++)
+		{
+			
+		}
+	}
+
+	public void replace_one_one_all()
+	{
+		
 	}
 	
 	public boolean checkValidBin(int bin_index, int item_index)
@@ -419,7 +397,11 @@ public class MixSolver {
 		MinMaxTypeMultiKnapsackInputBin old_bin = this.bins[old_bin_index];
 		this.current_w[old_bin_index] -= item.getW();
 		this.current_p[old_bin_index] -= item.getP();
-		this.low_w_needed[old_bin_index] = old_bin.getMinLoad() + item.getW();
+//		this.low_w_needed[old_bin_index] = old_bin.getMinLoad() + item.getW();
+		if(this.current_w[old_bin_index] == 0)
+			this.low_w_needed[old_bin_index] = 0;
+		else
+			this.low_w_needed[old_bin_index] = old_bin.getMinLoad() - this.current_w[old_bin_index];
 		this.current_bin_type[old_bin_index][item.getT()] -= 1;
 		this.current_bin_class[old_bin_index][item.getR()] -= 1;
 		if(this.current_bin_type[old_bin_index][item.getT()] == 0)
@@ -470,129 +452,25 @@ public class MixSolver {
         	}
         }
         this.list_items_unpack = this.violation_item;
+        this.current_num_item = this.n_items - this.violation_item.size();
 	}
 	
-	public void printSolution(int[] a)
+	public void search()
 	{
-		for(int bin: a)
-			System.out.print(bin + ",");
-		int result = this.n_items - this.violation_item.size();
-		System.out.println("\nMax item: " + result);
-	}
-	
-	public void printMap()
-	{
-		System.out.println("Type:");
-		for(Integer key: map_item_type.keySet())
-		{
-			System.out.print(key + ": ");
-			for(Integer item: map_item_type.get(key))
-			{
-				System.out.print(item + " ");
-			}
-			System.out.print("\n");
-		}
-		System.out.println("Class:");
-		for(Integer key: map_item_class.keySet())
-		{
-			System.out.print(key + ": ");
-			for(Integer item: map_item_class.get(key))
-			{
-				System.out.print(item + " ");
-			}
-			System.out.print("\n");
-		}
-		
-	}
-	
-	public int checkSolution(int[] solution, MinMaxTypeMultiKnapsackInput inputData, int max_num_type, int max_num_class)
-	{
-		MinMaxTypeMultiKnapsackInputBin[] bins = inputData.getBins();
-		MinMaxTypeMultiKnapsackInputItem[] items = inputData.getItems();
-		int n_bins = bins.length;
-		int n_items = items.length;
-		double[] cr_w = new double[n_bins];
-		double[] cr_p = new double[n_bins];
-		int[] cr_type = new int[n_bins];
-		int[] cr_class = new int[n_bins];
-		int[][] cr_bin_type = new int[n_bins][max_num_type+1];
-		int[][] cr_bin_class = new int[n_bins][max_num_class+1];
-
-		for(int item = 0; item < n_items; item++)
-		{
-			if(solution[item] != n_bins)
-			{
-				cr_w[solution[item]] += items[item].getW();
-				cr_p[solution[item]] += items[item].getP();
-				if(cr_bin_type[solution[item]][items[item].getT()] == 0)
-				{
-					cr_bin_type[solution[item]][items[item].getT()] = 1;
-					cr_type[solution[item]] += 1;
-				}
-				if(cr_bin_class[solution[item]][items[item].getR()] == 0)
-				{
-					cr_bin_class[solution[item]][items[item].getR()] = 1;
-					cr_class[solution[item]] += 1;
-				}
-			}
-			
-		}
-		for(int bin = 0; bin < n_bins; bin++)
-		{
-			if(cr_w[bin] > bins[bin].getCapacity() && bins[bin].getCapacity() > 0)
-			{
-				System.out.print(bin+" " + cr_w[bin] + "/" + bins[bin].getCapacity() );
-				return 1;
-			}
-			if(cr_w[bin] > 0 && cr_w[bin] < bins[bin].getMinLoad())
-			{
-				System.out.print(bin+": " + cr_w[bin] + "/" + bins[bin].getMinLoad());
-				return 2;
-			}
-			if(cr_p[bin] > bins[bin].getP() && bins[bin].getP() > 0)
-			{
-				System.out.print(bin+" " + cr_p[bin] + "/" + bins[bin].getP() );
-				return 3;
-			}
-			if(cr_type[bin] > bins[bin].getT())
-				return 4;
-			if(cr_class[bin] > bins[bin].getR())
-			{
-				System.out.print(bin);
-				return 5;
-			}
-		}
-		return 0;
-		
-	}
-	
-	public void printInter()
-	{
-		int ni = 0; //num_item
-		for(Integer key_type: map_item_type.keySet())
-		{
-			Set<Integer> list_items_type = map_item_type.get(key_type);
-			for(Integer key_class: map_item_class.keySet())
-			{
-				System.out.print(key_type + "," + key_class + ": ");
-				Set<Integer> list_items_class = map_item_class.get(key_class);
-				Set<Integer> current_item = new HashSet<Integer>(list_items_type);
-				current_item.retainAll(list_items_class);
-				ni += current_item.size();
-				for(Integer item: current_item)
-				{
-					System.out.print(item + ",");
-				}
-				System.out.print("\n");
-			}
-			System.out.print("\n");
-		}
-		System.out.println("NI: " + ni);
+    	this.hillClimbing();
+    	this.updateViolation();
+       	for(int i = 0; i < 10; i++)
+    	{
+    		System.out.println("Step: " + i);
+    		this.add_item_to_bin();
+			this.updateViolation();
+    	}
 	}
 	
 	public static void main(String[] args) {
 		//Load data
 		MinMaxTypeMultiKnapsackInput inputData = new MinMaxTypeMultiKnapsackInput();
+		Util utils = new Util();
 		inputData = inputData.loadFromFile("data/sample.json");
 //		inputData = inputData.loadFromFile("data/MinMaxTypeMultiKnapsackInput.json");
 //		inputData = inputData.loadFromFile("data/MinMaxTypeMultiKnapsackInput-1000.json");
@@ -601,20 +479,13 @@ public class MixSolver {
 		//Search
 		MixSolver solver = new MixSolver(inputData);
 		System.out.println("Searching....");
-    	solver.greedy_search();
-    	solver.hillClimbing();
-		solver.updateViolation();
-       	for(int i = 0; i < 10; i++)
-    	{
-    		System.out.println("Step: " + i);
-    		solver.add_item_to_bin();
-			solver.updateViolation();
-    	}
-    	solver.updateViolation();
-        //Print
-        solver.printSolution(solver.X);
+		solver.greedy_search();
+		solver.search();
+		
+		//Print
+        utils.printSolution(solver.X, solver.current_num_item);
         System.out.println("\nDone! \nError code: " + 
-        		solver.checkSolution(solver.X, inputData, solver.max_num_type, solver.max_num_class) + 
+        		utils.checkSolution(solver.X, inputData, solver.max_num_type, solver.max_num_class) + 
         		" Unpack: " + solver.violation_item.size() + " Empty bin: " + solver.empty_bin.size()
         		);     
 	}
