aef1a7d63aab92f35e286521e2c491e9d18fbfa3
dungkhmt <dungkhmt@gmail.com>
2020-03-11 16:30:33 +0700

    init class

---
+++ OpenCBLS/src/cbls115676khmt61/phamquangdung/QueenCBLS.java
@@ -0,0 +1,70 @@
+package cbls115676khmt61.phamquangdung;
+
+
+
+import java.util.HashMap;
+
+import localsearch.applications.queen.Queen;
+import localsearch.constraints.alldifferent.AllDifferent;
+import localsearch.functions.basic.FuncPlus;
+import localsearch.model.ConstraintSystem;
+import localsearch.model.IFunction;
+import localsearch.model.LocalSearchManager;
+import localsearch.model.VarIntLS;
+import localsearch.selectors.MinMaxSelector;
+
+public class QueenCBLS {
+
+	/**
+	 * @param args
+	 */
+	public void test1(){
+		int n=1000;
+		
+		LocalSearchManager ls=new LocalSearchManager();
+		ConstraintSystem S=new ConstraintSystem(ls);
+		
+		VarIntLS[] x = new VarIntLS[n];
+		for (int i = 0; i < n; i++){
+			x[i] = new VarIntLS(ls, 0, n - 1);
+		}
+		
+		S.post(new AllDifferent(x));
+		
+		IFunction[] f1=new IFunction[n];
+		for (int i = 0; i < n; i++) 
+			f1[i] =  new FuncPlus(x[i], i);
+		S.post(new AllDifferent(f1));
+		
+		IFunction[] f2 = new IFunction[n];
+		for (int i = 0; i < n; i++) f2[i] = new FuncPlus(x[i], -i);
+		S.post(new AllDifferent(f2));
+		
+		ls.close();
+		System.out.println("Init S = " + S.violations());
+		MinMaxSelector mms = new MinMaxSelector(S);
+		
+		int it = 0;
+		while(it < 10000 && S.violations() > 0){
+			
+			VarIntLS sel_x = mms.selectMostViolatingVariable();
+			int sel_v = mms.selectMostPromissingValue(sel_x);
+			
+			sel_x.setValuePropagate(sel_v);
+			System.out.println("Step " + it + ",  S = " + S.violations());
+			
+			it++;
+		}
+		
+		
+		System.out.println(S.violations());
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		QueenCBLS Q = new QueenCBLS();
+		Q.test1();
+	}
+
+}
