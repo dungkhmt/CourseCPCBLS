6a14e95478088de71b9085803f3c4430cac5b5e5
nguyenthienhy <37505673+nguyenthienhy@users.noreply.github.com>
2020-03-20 12:19:51 +0700

    format

--- OpenCBLS/src/cbls115676khmt61/Nguyen_Thien_Hy-20162111/OpenCBLS/src/Baitap/NQueen.java
+++ OpenCBLS/src/cbls115676khmt61/Nguyen_Thien_Hy-20162111/OpenCBLS/src/Baitap/NQueen.java
@@ -9,53 +9,59 @@ import localsearch.model.VarIntLS;
 import localsearch.selectors.MinMaxSelector;
 
 public class NQueen {
-	int n; // number of queens
-	LocalSearchManager mgr;
-	VarIntLS[] x;// decision variables
-	ConstraintSystem S;
-	public NQueen(int n) {
-		this.n = n;
-	}
-	public void stateModel() {
-		mgr = new LocalSearchManager();
-		x = new VarIntLS[n];
-		for(int i = 0 ; i < n ; i ++) {
-			x[i] = new VarIntLS(mgr, 0 , n - 1);
-		}
-		S = new ConstraintSystem(mgr);
-		S.post(new AllDifferent(x));
-		IFunction[] f1 = new IFunction[n];
-		for(int i = 0 ; i < n ; i ++) {
-			f1[i] = new FuncPlus(x[i] , i);
-		}
-		S.post(new AllDifferent(f1));
-		IFunction[] f2 = new IFunction[n];
-		for(int i = 0 ; i < n ; i ++) {
-			f2[i] = new FuncPlus(x[i] , -i);
-		}
-		S.post(new AllDifferent(f2));
-		mgr.close();
-	}
-	public void search() {
-		int it = 0;
-		while(it < 100000 && S.violations() > 0) {
-			MinMaxSelector mms = new MinMaxSelector(S);
-			VarIntLS sel_x = mms.selectMostViolatingVariable();
-			int sel_v = mms.selectMostPromissingValue(sel_x);
-			sel_x.setValuePropagate(sel_v);// local move
-			System.out.println("Step " + it + ", S = " + S.violations());
-			it ++;
-		}
-		printSol();
-	}
-	public void printSol() {
-		for(int i = 0 ; i < n ; i ++) {
-			System.out.print(x[i].getValue() + " ");
-		}
-	}
-	public static void main(String[] args) {
-		NQueen ob = new NQueen(700);
-		ob.stateModel();
-		ob.search();
-	}
+
+    int n; // number of queens
+    LocalSearchManager mgr;
+    VarIntLS[] x;// decision variables
+    ConstraintSystem S;
+
+    public NQueen(int n) {
+        this.n = n;
+    }
+
+    public void stateModel() {
+        mgr = new LocalSearchManager();
+        x = new VarIntLS[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = new VarIntLS(mgr, 0, n - 1);
+        }
+        S = new ConstraintSystem(mgr);
+        S.post(new AllDifferent(x));
+        IFunction[] f1 = new IFunction[n];
+        for (int i = 0; i < n; i++) {
+            f1[i] = new FuncPlus(x[i], i);
+        }
+        S.post(new AllDifferent(f1));
+        IFunction[] f2 = new IFunction[n];
+        for (int i = 0; i < n; i++) {
+            f2[i] = new FuncPlus(x[i], -i);
+        }
+        S.post(new AllDifferent(f2));
+        mgr.close();
+    }
+
+    public void search() {
+        int it = 0;
+        while (it < 100000 && S.violations() > 0) {
+            MinMaxSelector mms = new MinMaxSelector(S);
+            VarIntLS sel_x = mms.selectMostViolatingVariable();
+            int sel_v = mms.selectMostPromissingValue(sel_x);
+            sel_x.setValuePropagate(sel_v);// local move
+            System.out.println("Step " + it + ", S = " + S.violations());
+            it++;
+        }
+        printSol();
+    }
+
+    public void printSol() {
+        for (int i = 0; i < n; i++) {
+            System.out.print(x[i].getValue() + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        NQueen ob = new NQueen(700);
+        ob.stateModel();
+        ob.search();
+    }
 }
--- OpenCBLS/src/cbls115676khmt61/Nguyen_Thien_Hy-20162111/OpenCBLS/src/Baitap/test.java
+++ OpenCBLS/src/cbls115676khmt61/Nguyen_Thien_Hy-20162111/OpenCBLS/src/Baitap/test.java
@@ -6,26 +6,29 @@ import localsearch.model.LocalSearchManager;
 import localsearch.model.VarIntLS;
 
 public class test {
-	LocalSearchManager mgr = new LocalSearchManager();
-	int n = 5;
-	int[] w = new int[] {3 , 2 , 5 , 4 , 6};
-	VarIntLS[] X  = new VarIntLS[n];
-	void init() {
-		for(int i = 0 ; i < n ; i ++) {
-			X[i] = new VarIntLS(mgr, 1 , 5);
-		}
-		X[0].setValuePropagate(2);
-		X[1].setValuePropagate(1);
-		X[2].setValuePropagate(5);
-		X[3].setValuePropagate(2);
-		X[4].setValuePropagate(3);
-		IFunction s = new ConditionalSum(X , w , 2); 
-		// tong trong so w[i] tai i sao cho X[i] = 2
-		mgr.close();
-		System.out.println("s = " + s.getValue());
-	}
-	public static void main(String[] args) {
-		test t = new test();
-		t.init();
-	}
+
+    LocalSearchManager mgr = new LocalSearchManager();
+    int n = 5;
+    int[] w = new int[]{3, 2, 5, 4, 6};
+    VarIntLS[] X = new VarIntLS[n];
+
+    void init() {
+        for (int i = 0; i < n; i++) {
+            X[i] = new VarIntLS(mgr, 1, 5);
+        }
+        X[0].setValuePropagate(2);
+        X[1].setValuePropagate(1);
+        X[2].setValuePropagate(5);
+        X[3].setValuePropagate(2);
+        X[4].setValuePropagate(3);
+        IFunction s = new ConditionalSum(X, w, 2);
+        // tong trong so w[i] tai i sao cho X[i] = 2
+        mgr.close();
+        System.out.println("s = " + s.getValue());
+    }
+
+    public static void main(String[] args) {
+        test t = new test();
+        t.init();
+    }
 }
