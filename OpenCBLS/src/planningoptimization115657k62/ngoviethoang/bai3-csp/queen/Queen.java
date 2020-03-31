b0e77e80c4d1b00881b57827ad32e335da2c94fa
deserteagle735 <deserteaglexiii@gmail.com>
2020-03-31 21:27:38 +0700

    add bai3-csp bcap+tsp+queen+sudoku

---
+++ OpenCBLS/src/planningoptimization115657k62/ngoviethoang/bai3-csp/queen/Queen.java
@@ -0,0 +1,32 @@
+package queen;
+
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.variables.IntVar;
+
+public class Queen {
+	public void queen(int n) {
+		Model model = new Model("Queen");
+		IntVar[] x = model.intVarArray("x",n, 0,n-1);
+		IntVar[] bslash = new IntVar[n];
+		IntVar[] fslash = new IntVar[n];
+		for (int i = 0; i < n; i++) {
+			bslash[i] = model.intOffsetView(x[i], i);
+			fslash[i] = model.intOffsetView(x[i], -i);
+		} 
+		
+		model.allDifferent(x).post();
+		model.allDifferent(bslash).post();
+		model.allDifferent(fslash).post();
+		
+		model.getSolver().solve();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println("X["+(i+1)+"] = "+x[i].getValue());
+		}
+	}
+	
+	public static void main(String args[]) {
+		Queen app = new Queen();
+		app.queen(100);
+	}
+}
