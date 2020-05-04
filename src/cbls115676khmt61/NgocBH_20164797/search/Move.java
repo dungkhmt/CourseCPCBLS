2ea5f936dc83b7b8eea47d205a36d5c519ace037
Hoainam25699 <Hoainam256@gmail.com>
2020-05-04 16:50:49 +0700

    update cp

--- src/cbls115676khmt61/NgocBH_20164797/search/Move.java
+++
@@ -1,5 +0,0 @@
-package cbls115676khmt61.ngocbh_20164797.search;
-
-public abstract class Move {
-    public boolean is_legal;
-}
--- src/localsearch/domainspecific/vehiclerouting/vrp/enums/PointType.java
+++
@@ -1,7 +0,0 @@
-package localsearch.domainspecific.vehiclerouting.vrp.enums;
-
-public enum PointType {
-	STARTING_ROUTE,
-	TERMINATING_ROUTE,
-	CLIENT
-}
