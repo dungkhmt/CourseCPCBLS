f576dc7e36e21eed7df6e66553ef1d6b3a3f77da
nguyenthienhy <37505673+nguyenthienhy@users.noreply.github.com>
2020-05-17 09:22:58 +0700

    update

--- src/cbls115676khmt61/Nguyen_Thien_Hy-20162111/OpenCBLS/src/MiniProject/GraphPartitioningCP.java
+++ src/cbls115676khmt61/Nguyen_Thien_Hy-20162111/OpenCBLS/src/MiniProject/GraphPartitioningCP.java
@@ -4,19 +4,18 @@ import org.chocosolver.solver.Model;
 import org.chocosolver.solver.variables.IntVar;
 
 public class GraphPartitioningCP {
-
-    private int N = 6;
+    private int N = 9;
     private int[][] E = {
-        {0 , 2 , 1},
-        {0 , 4 , 3},
-        {0 , 5 , 2},
-        {1 , 4 , 4},
-        {1 , 5 , 3},
-        {2 , 3 , 5},
-        {2 , 4 , 1},
-        {4 , 5 , 5}
+        {0 , 6 , 3},
+        {0 , 8 , 4},
+        {1 , 3 , 2},
+        {1 , 5 , 2},
+        {2 , 4 , 3},
+        {2 , 7 , 4},
+        {4 , 8 , 5},
+        {5 , 6 , 4},
     };
-
+    
     public void solve() {
         int[] c = new int[E.length];
         int C = 0;
@@ -24,48 +23,59 @@ public class GraphPartitioningCP {
             c[i] = E[i][2];
             C += c[i];
         }
-
+        
         int K = 3; 
         
         Model model = new Model("graph partitioning");
-        IntVar[] z = new IntVar[E.length];
-        IntVar[] x = new IntVar[N];
+        IntVar[] e = new IntVar[E.length];
+        IntVar[][] x = new IntVar[N][K];
 
-        IntVar obj = model.intVar("obj", 0, C);
+        IntVar obj = model.intVar("obj" , 0 , C);
         for (int k = 0; k < E.length; k++) {
-            z[k] = model.intVar("z[" + k + "]", 0 , 1);
+            e[k] = model.intVar("e[" + k + "]", 0 , 1);
         }
         for (int i = 0; i < N; i++) {
-            x[i] = model.intVar("x[" + i + "]", 0 , K - 1);
+            for(int k = 0 ; k < K ; k ++){
+                x[i][k] = model.intVar("x[" + i + "][" + k + "]", 0 , 1);
+            }
         }
-        for (int k = 0; k < E.length; k++) {
-            model.ifThen(model.arithm(z[k], "=", 1), model.arithm(x[E[k][0]], "-", x[E[k][1]], "!=", 0));
-            model.ifThen(model.arithm(x[E[k][0]], "-", x[E[k][1]], "!=", 0), model.arithm(z[k], "=", 1));
+        for (int i = 0; i < E.length; i ++) {
+            for(int k = 0 ; k < K ; k ++){
+                model.arithm(e[i] , ">=" , x[E[i][0]][k], "-", x[E[i][1]][k]);
+                model.arithm(e[i] , ">=" , x[E[i][1]][k], "-", x[E[i][0]][k]);
+                model.arithm(x[E[i][1]][k] , "+" , x[E[i][0]][k] , "<=" , e[i]);
+            }
         }
-        int[] one = new int[N];
-        for (int i = 0; i < N; i++) {
+        
+        int[] one = new int[K];
+        for (int i = 0; i < K; i ++) {
             one[i] = 1;
         }
-        model.scalar(x, one, "=", N * (K - 1) / 2).post();
-
-        model.scalar(z, c, "=", obj).post();
+        
+        for(int v = 0 ; v < N ; v ++){
+            model.scalar(x[v], one, "=" , 1).post();
+        }
+        
+        model.scalar(e , c , "=", obj).post();
 
         model.setObjective(model.MAXIMIZE, obj);
         model.getSolver().solve();
         model.getSolver().printStatistics();
         
+        
         for (int i = 0; i < N; i++) {
-            System.out.println("x[" + i + "] = " + x[i].getValue());
-        }
-        for (int k = 0; k < E.length; k++) {
-            System.out.println("z[" + E[k][0] + "," + E[k][1] + "] = " + z[k].getValue());
+            System.out.println("e[" + i + "] = " + e[i].getValue());
         }
         
+        for (int i = 0; i < N; i++) {
+            for(int j = 0 ; j < K ; j ++){
+                System.out.println("x[" + i + "][" + j + "] = " + x[i][j].getValue());
+            }
+        }
         System.out.println("obj = " + obj.getValue());
     }
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         GraphPartitioningCP app = new GraphPartitioningCP();
         app.solve();
     }
