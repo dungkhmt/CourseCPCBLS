954f03b30c3ad2b1323e9774b93a51e9c7949948
hung <tranhuyhung1998@gmail.com>
2020-05-22 00:22:29 +0700

    update 2

--- src/cbls115676khmt61/TranHuyHung_20164777/BACP.java
+++ src/cbls115676khmt61/TranHuyHung_20164777/BACP.java
@@ -1,85 +1,87 @@
 package cbls115676khmt61.TranHuyHung_20164777;
 
-import localsearch.constraints.basic.LessOrEqual;
-import localsearch.constraints.basic.LessThan;
-import localsearch.functions.conditionalsum.ConditionalSum;
-import localsearch.model.ConstraintSystem;
 import localsearch.model.IConstraint;
+import localsearch.constraints.basic.LessOrEqual;
+import localsearch.functions.conditionalsum.ConditionalSum;
+import localsearch.constraints.basic.LessThan;
 import localsearch.model.IFunction;
-import localsearch.model.LocalSearchManager;
+import localsearch.model.ConstraintSystem;
 import localsearch.model.VarIntLS;
+import localsearch.model.LocalSearchManager;
 
-public class BACP {
-	int N  = 12; // so mon hoc 0,1,2,...
-	int P = 4; // so hoc ky 0,1,2,...
-	int[] credits = {2, 1, 2, 1, 3, 2, 1, 3, 2, 3, 1, 3};
-	int[][] pre = {
-			{1, 0},
-			{5, 8},
-			{4, 5},
-			{4, 7},
-			{3, 10},
-			{5, 11}
-	};
-	int a = 3, b = 3, c = 5, d = 7;
-	
-	// model
-	LocalSearchManager mgr;
-	VarIntLS[] X;
-	ConstraintSystem S;
-	IFunction[] numberCoursesPeriod; //so mon hoc moi hoc ky
-	IFunction[] numberCreditsPeriod; //so tin chi cac mon hoc moi hoc ky
-	
-	public void stateModel() {
-		mgr = new LocalSearchManager();
-		X = new VarIntLS[N];
-		for (int i=0; i<N; i++)
-			X[i] = new VarIntLS(mgr, 0, P-1);
-		
-		S = new ConstraintSystem(mgr);
-		
-		for (int k=0; k<pre.length; k++) {
-			IConstraint c = new LessThan(X[pre[k][0]], X[pre[k][1]]);
-			S.post(c);
-		}
-		
-		numberCoursesPeriod = new IFunction[P];
-		numberCreditsPeriod = new IFunction[P];
-		
-		for (int j=0; j<P; j++) {
-			numberCoursesPeriod[j] = new ConditionalSum(X, j);
-			numberCreditsPeriod[j] = new ConditionalSum(X, credits, j);
-			
-			S.post(new LessOrEqual(numberCoursesPeriod[j], b));
-			S.post(new LessOrEqual(a, numberCoursesPeriod[j]));
-			S.post(new LessOrEqual(numberCreditsPeriod[j], d));
-			S.post(new LessOrEqual(c, numberCreditsPeriod[j]));
-		}
-		
-		// S.post(new LessOrEqual(new MySum(X), 12));
-		mgr.close(); // create dependency graph
-	}
-	
-	public void search() {
-		HillClimbingSearch searcher = new HillClimbingSearch();
-		searcher.search(S, 10000);
-	}
-	
-	public void printResult() {
-		for (int j=0; j<P; j++) {
-			System.out.print("HK " + j + ": ");
-			for (int i=0; i<N; i++)
-				if (X[i].getValue() == j)
-					System.out.print(i + ", ");
-			System.out.print("number courses = " + numberCoursesPeriod[j].getValue());
-			System.out.println(", number credits = " + numberCreditsPeriod[j].getValue());
-		}
-	}
-	
-	public static void main(String[] args) {
-		BACP app = new BACP();
-		app.stateModel();
-		app.search();
-		app.printResult();
-	}
+public class BACP
+{
+    int N;
+    int P;
+    int[] credits;
+    int[][] pre;
+    int a;
+    int b;
+    int c;
+    int d;
+    LocalSearchManager mgr;
+    VarIntLS[] X;
+    ConstraintSystem S;
+    IFunction[] numberCoursesPeriod;
+    IFunction[] numberCreditsPeriod;
+    
+    public BACP() {
+        this.N = 12;
+        this.P = 4;
+        this.credits = new int[] { 2, 1, 2, 1, 3, 2, 1, 3, 2, 3, 1, 3 };
+        this.pre = new int[][] { { 1, 0 }, { 5, 8 }, { 4, 5 }, { 4, 7 }, { 3, 10 }, { 5, 11 } };
+        this.a = 3;
+        this.b = 3;
+        this.c = 5;
+        this.d = 7;
+    }
+    
+    public void stateModel() {
+        this.mgr = new LocalSearchManager();
+        this.X = new VarIntLS[this.N];
+        for (int i = 0; i < this.N; ++i) {
+            this.X[i] = new VarIntLS(this.mgr, 0, this.P - 1);
+        }
+        this.S = new ConstraintSystem(this.mgr);
+        for (int k = 0; k < this.pre.length; ++k) {
+            final IConstraint c = (IConstraint)new LessThan(this.X[this.pre[k][0]], this.X[this.pre[k][1]]);
+            this.S.post(c);
+        }
+        this.numberCoursesPeriod = new IFunction[this.P];
+        this.numberCreditsPeriod = new IFunction[this.P];
+        for (int j = 0; j < this.P; ++j) {
+            this.numberCoursesPeriod[j] = (IFunction)new ConditionalSum(this.X, j);
+            this.numberCreditsPeriod[j] = (IFunction)new ConditionalSum(this.X, this.credits, j);
+            this.S.post((IConstraint)new LessOrEqual(this.numberCoursesPeriod[j], this.b));
+            this.S.post((IConstraint)new LessOrEqual(this.a, this.numberCoursesPeriod[j]));
+            this.S.post((IConstraint)new LessOrEqual(this.numberCreditsPeriod[j], this.d));
+            this.S.post((IConstraint)new LessOrEqual(this.c, this.numberCreditsPeriod[j]));
+        }
+        this.mgr.close();
+    }
+    
+    public void search() {
+        final MyTabuSearch ts = new MyTabuSearch((IConstraint)this.S);
+        ts.search(100000, 20, 100);
+    }
+    
+    public void printResult() {
+        for (int j = 0; j < this.P; ++j) {
+            System.out.print("HK " + j + ": ");
+            for (int i = 0; i < this.N; ++i) {
+                if (this.X[i].getValue() == j) {
+                    System.out.print(String.valueOf(i) + ", ");
+                }
+            }
+            System.out.print("number courses = " + this.numberCoursesPeriod[j].getValue());
+            System.out.println(", number credits = " + this.numberCreditsPeriod[j].getValue());
+        }
+    }
+    
+    public static void main(final String[] args) {
+        final BACP app = new BACP();
+        app.stateModel();
+        app.search();
+        app.printResult();
+    }
 }
