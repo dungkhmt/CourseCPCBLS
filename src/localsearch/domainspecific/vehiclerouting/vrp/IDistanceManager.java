0fc307c28019a2e666a2dd17d3d54b94db1d8fe5
dungkhmt <dungkhmt@gmail.com>
2020-09-29 07:27:34 +0700

    clean all

--- src/localsearch/domainspecific/vehiclerouting/vrp/IDistanceManager.java
+++
@@ -1,6 +0,0 @@
-package localsearch.domainspecific.vehiclerouting.vrp;
-
-import localsearch.domainspecific.vehiclerouting.vrp.entities.*;
-public interface IDistanceManager {
-	public double getDistance(Point x, Point y);
-}
--- src/localsearch/domainspecific/vehiclerouting/vrp/online/functions/OFunctionVR.java
+++
@@ -1,8 +0,0 @@
-package localsearch.domainspecific.vehiclerouting.vrp.online.functions;
-
-import localsearch.domainspecific.vehiclerouting.vrp.IFunctionVR;
-import localsearch.domainspecific.vehiclerouting.vrp.online.invariants.OInvariantVR;
-
-public interface OFunctionVR extends IFunctionVR, OInvariantVR {
-	
-}
